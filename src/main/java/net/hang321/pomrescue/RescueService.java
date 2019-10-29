0cb32475b4e48f1fa21529702d44776bfe1a12e4
Steve Chan <hang321+github@gmail.com>
2019-10-29 23:35:33 +1100

    init app shell

---
+++ src/main/java/net/hang321/pomrescue/RescueService.java
@@ -0,0 +1,16 @@
+package net.hang321.pomrescue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RescueService {
+
+	private static Logger logger = LoggerFactory.getLogger(RescueService.class);
+
+	public void rescue() {
+		// TODO implementation
+
+	}
+}
